d7ba0dbca7059b85ef6584a4926bc2110253e9ae
katesawtell <katesawtell99@gmail.com>
2025-02-22 14:02:01 -0800

    added tables and filler info

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Farm Fetch' });
+  res.render('index', { title: 'Farm Fetch.' });
 });
 
 router.get('/foolhardy', (req, res) => {
-  res.render('foolhardy');
+  res.render('foolhardy', {vendor_name: 'Foolhardy'});
+});
+
+router.get('/harvest_root', (req, res) => {
+  res.render('harvest_root', {vendor_name: 'Harvest Root Organics'});
 });
 
 module.exports = router;
